252482f54fd5361c4356e16766cf4636c027d176
Công Huy <71679454+conghuy299@users.noreply.github.com>
2023-08-18 15:36:14 +0700

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,56 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EmployeeUpdateComponent } from './employee-update/employee-update.component';
+import { DatePipe } from '@angular/common';
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NavbarComponentTsComponent } from './navbar.component.ts/navbar.component.ts.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { EmployeeProjectComponent } from './employee-project/employee-project.component';
+import { ProjectAddComponent } from './project-add/project-add.component';
+import { ProjectUpdateComponent } from './project-update/project-update.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeAddComponent,
+    EmployeeUpdateComponent,
+    ConfirmationDialogComponent,
+    NavbarComponentTsComponent,
+    ProjectListComponent,
+    EmployeeProjectComponent,
+    ProjectAddComponent,
+    ProjectUpdateComponent,
+
+
+
+
+
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    NgxPaginationModule
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DatePipe],
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
